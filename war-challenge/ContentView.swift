2c5438eb58c05d7d4cb6264b9656197b37476790
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-01-11 23:44:17 -0500

    Build UI

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -9,7 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello!")
+        
+        ZStack {
+            
+            Image("background")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
+    
+        
     }
 }
 

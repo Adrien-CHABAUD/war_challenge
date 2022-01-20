020b0b2dc3ecbaf00ab1637a3835f2666f2e744c
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-01-20 15:26:50 -0500

    State Properties + Logic

--- war-challenge/ContentView.swift
+++ war-challenge/ContentView.swift
@@ -8,31 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // Properties
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         
         ZStack {
             
+            // Background
             Image("background")
                 .ignoresSafeArea()
             
+            // Cards
             VStack {
                 Spacer()
                 Image("logo")
                 Spacer()
                 HStack {
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
                 
-                Button(action: {}, label: {
+                // Deal Button
+                Button(action: {
+                    
+                    // Update the cards
+                    
+                    // Generate a random number between 2-14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // Update the score
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand {
+                        cpuScore += 1
+                    }
+                    
+                }, label: {
                     Image("dealbutton")
                 })
                 
                 Spacer()
+                
+                // Score Display
                 HStack {
                     Spacer()
                     VStack {
@@ -40,7 +72,7 @@ struct ContentView: View {
                             .font(.headline)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
@@ -50,7 +82,7 @@ struct ContentView: View {
                             .font(.headline)
                             .foregroundColor(Color.white)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                             .foregroundColor(Color.white)
                     }
